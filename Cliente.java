91f0aa49191bc57e3b6ffbc1c40e71a49b319ca5
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2021-10-10 01:06:14 -0300

    Adicionando classe Cliente

---
+++ Cliente.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jacyirice
+ */
+public class Cliente {
+    private int idCliente;
+    private String nomeCliente;
+    private String enderecoCliente;
+    private String cpfCliente;
+    
+    public void CadastraCliente(){}
+    public void VisualizaCliente(){}
+    public void AlteraCliente(){}
+}

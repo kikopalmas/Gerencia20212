69df9e24a03094f14885a942692e4da9e25aeaa2
jklea <jacykleasilvaoliveira@gmail.com>
2021-10-12 21:36:30 -0300

    Criando FormaPagamento

---
+++ FormaPagamento.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jacykléa
+ */
+public class FormaPagamento {
+    private int idFormaPgto;
+    private String formaPgto;
+    private String descricao;
+    
+    public void escolheFormaPGto(){}
+    public void retomaFormaPGto(){}
+    public void insereFormaPGto(){}
+    public void removeFormaPGto(){}
+    public void enviaValorPedido(){}
+    public void calculaPedido(){}
+}

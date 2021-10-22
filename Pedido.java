b08c229a2607f59ac6a000f6c304cca1aa13e932
warius <warius@whalyson>
2021-10-22 11:14:41 -0300

    add class Pedido

---
+++ Pedido.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author warius
+ */
+public class Pedido {
+
+    private int idPedido;
+    private int idCliente;
+    private int idCarrinho;
+    private boolean orcamento;
+    
+    public void calculaTotalCarrinho(){}
+    
+    public void adicionaProduto(int idProduto, int quantidade){}
+    
+    public void retiraProduto(int idProduto){}
+    
+    public void orcamentoParaPedido(int idPedido){}
+    
+    public void enviaValorProdutos(int idPedido){}
+    
+    public void concluiPedido(int idPedido){}
+    
+    public void pagtoStatus(int idPedido){}
+}

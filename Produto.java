84e9db1e0d3733395447de999e0262066bdd863b
Wellikson <48630524+Wellikson@users.noreply.github.com>
2022-04-13 11:39:25 -0300

    Atividade de Desenvolvimento Colaborativo

---
+++ Produto.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author wellikson
+ */
+public class Produto {
+
+    private int idProduto;
+    private String nomeProduto;
+    private int pesoProduto;
+
+    private void cadastrarProduto() {
+    };
+    private void removeProduto(int idProduto) {
+    };
+    private void alterarProduto(int idProduto) {
+    };
+}
